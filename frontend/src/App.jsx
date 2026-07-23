1c0681d8fad751073f465b27ab3537f76921392f
AfifaHaque <afifa.haq2013@gmail.com>
2026-07-23 20:04:43 +0600

    Remove login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import Login from "./pages/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
@@ -21,7 +20,7 @@ function App() {
           <Navbar />
 
           <Routes>
-            <Route path="/login" element={<Login />} />
+            
             <Route path="/" element={<Home />} />
             <Route path="/tasks" element={<TaskManagement />} />
             <Route path="/study-schedule" element={<StudySchedule />} />
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -6,8 +6,7 @@ function Sidebar() {
       <h2 className="logo">Study Planner</h2>
 
       <nav className="sidebar-menu">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
+        <Link to="/">Home</Link>        
         <Link to="/tasks">Tasks</Link>
         <Link to="/study-schedule">Study Schedule</Link>
         <Link to="/calendar">Calendar</Link>
